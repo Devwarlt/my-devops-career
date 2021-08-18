c1353f9639f8339804c38d840c25ad46957b1274
Nádio Pontes <nadio.pontes@redes.unb.br>
2021-08-18 12:23:25 -0300

    Update old-ubuntu-update.sh

--- old-ubuntu-update.sh
+++ old-ubuntu-update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo sed -i -re 's/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
+sudo sed -i -e 's/archive.ubuntu.com\|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 sudo apt-get update && sudo apt-get dist-upgrade
